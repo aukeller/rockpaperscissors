325a9634dbaaaa277a8ffe573e03f901f2dd4f7e
Austin Keller <austinkllr@gmail.com>
2021-11-29 21:10:03 -0800

    added buttons for playing round, and css

--- rps.js
+++ rps.js
@@ -13,33 +13,34 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     
-    if (playerSelection === "rock") {
-        if (computerSelection === 'rock') {
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
             return "It's a tie.";
-        } else if (computerSelection === "paper") {
+        } else if (computerSelection == "paper") {
             return "You Lose! Paper beats rock";
         } else {
             return "You win! Rock beats scissors";
         }
 
-    } else if (playerSelection === "paper") {
-        if (computerSelection === 'paper') {
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "paper") {
             return "It's a tie.";
-        } else if (computerSelection === "rock") {
+        } else if (computerSelection == "rock") {
             return "You win! Paper beats rock";
         } else {
             return "You lose! Scissors beats paper";
         }
     } else {
-        if (computerSelection === 'scissors') {
+        if (computerSelection == "scissors") {
             return "It's a tie.";
-        } else if (computerSelection === "paper") {
+        } else if (computerSelection == "paper") {
             return "You Win! Scisssors beats paper";
         } else {
             return "You Lose! Rock beats scissors";
         }
     }
 }
+
 
 function declareWinner(playerScore, computerScore) {
     if (playerScore > computerScore) {
@@ -55,29 +56,40 @@ function game() {
     let playerScore = 0;
     let computerScore = 0;
     let round = 1;
+    let roundResult;
 
-    while (round <= 5) {
-        const playerSelection = prompt("Rock, paper, or scissors?");
-        const computerSelection = computerPlay();
+    const btns = document.querySelectorAll('button');
+    
 
-        let result = playRound(playerSelection, computerSelection);
+    btns.forEach(btn => {
+        btn.addEventListener('click', function(e) {
+            const computerSelection = computerPlay();
 
-        console.log(result);
+            roundResult = playRound(e.target.id, computerSelection);
+            
+            roundResult = roundResult.toLowerCase();
+            
+            if (roundResult.includes('you win')) {
+                playerScore += 1;
+                console.log("One point to you");
+            } else if (roundResult.includes('you lose')) {
+                computerScore += 1;
+                console.log("One point to computer");
+            } else {
+                console.log('Tie');
+            }
+            
+            round++;
+        });
+    });
 
-        result = result.toLowerCase();
-        if (result.includes('you win')) {
-            playerScore += 1;
-            console.log("One point to you");
-        } else if (result.includes('you lose')) {
-            computerScore += 1;
-            console.log("One point to computer");
-        }
 
-        round++;
-    }
+ 
+    
 
-    declareWinner(playerScore, computerScore);
+    
 
 }
+
 
 game();
